dfa6015367f4d8bcf006fddf08a55a505d5f6604
rjeetgupta <ranjeeturd@gmail.com>
2024-06-26 10:38:47 +0530

    Insertion sort code bug fix

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -4,8 +4,7 @@ using namespace std;
 void insertion_sort(int arr[], int n){
     for (int i = 0; i <= n - 1; i++){
         int j = i;
-        for (j > 0 && arr[j - 1] > arr[j])
-        {
+        while( j > 0 && arr[j-1] > arr[j]){
             int temp = arr[j - 1];
             arr[j - 1] = arr[j];
             arr[j] = temp;
@@ -18,7 +17,8 @@ int main(){
     int n;
     cin >> n;
     int arr[n];
-    insertion_sort(arr, n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+        insertion_sort(arr, n);
     for (int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
